5b48510762044212b68fc3d1f599c3f6410d464d
88rohit <91779710+88rohit@users.noreply.github.com>
2023-03-04 00:20:45 +0530

    first commit

---
+++ Day1.py
@@ -0,0 +1,99 @@
+# print("Hello,world")
+#this is the first comments
+
+#variables
+# x = 5
+# y = 4;
+# print(x)
+# print(y)
+
+# x =5
+# x = "rohit"
+# print(x)
+
+#check which type of variable
+# x =1
+# y = "rohit"
+# print(type(x))
+# print(type(y))
+
+#overwrite file or not 
+# a = 4
+# A = "rohit"
+# print(A)
+# print(a)
+# assign multiple value
+# x,y,z = "orange" , "Banana" , "cherry"
+# print(x)
+# print(y)
+# print(z)
+# fruits = ["apple" ,"banana" , "cherry"]
+# x,y,z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = "python"
+# y = "is"
+# z = "awesome"
+# print(x + " " + y + z)
+# python datatype
+#int float complex str list 
+# x = "hello world"
+# x = 20
+# x = 20.5
+
+# x = ["apple" , "orange" , "cherry"]
+# x = ("apple" , "banana" , "cherry")
+# x = range(6)
+# x = {"apple" , "banana" , "cherry"}
+# x = True
+# x = int(1)
+# print(x)
+# print(10 >9)
+# print(10 == 9)
+
+# a = 100
+# b = 22
+# if(b>a):
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
+
+#     x = "Hello"
+#     y = 15
+#     print(bool(x))
+#     print(bool(y))
+
+# l1 = ["apple" , "banana" , "cherry"]
+# l2 = [1 , 2, 3 , 4, 5]
+# l3 = [True, False , False]
+# print(l1)
+# print(l2)
+# print(l3)
+# print(type(l1))
+# print(type(l2))
+
+# l1 = ["apple" , "banana" , "cherry"]
+# l1.insert(2, "water")
+# print(l1)
+#if else statementb = 11
+# a= 1
+# b=1
+# if b>a:
+#     print("b is greater than a")
+# else:
+#     print("a is equal are b")
+
+
+# def my_function():
+#     print("Hello world to all")
+#     my_function()
+
+#     def my_function(country = "Norway"):
+#         print("T am from " + country)
+#         my_function("india")
+#         my_function("sweden")
+    
+
+
